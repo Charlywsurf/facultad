14332419974c4f05cc4b66086b0e93d607b90ab9
Carlos Giba <csgiba@gmail.com>
2023-07-20 00:46:40 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*eslint-env es6*/
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
